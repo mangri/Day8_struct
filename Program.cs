c0122beae61276592c700e7fb44a2d395bdff57f
mangri <fizfakas@gmail.com>
2022-09-18 13:44:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // if(pirkimoTipas.GetType != typeof(1))
+            int tickets10 = 0;
+            int tickets20 = 0;
+            int tickets30 = 0;
+            Console.WriteLine("Meniu:");
+            Console.WriteLine("[1] Buy tickets");
+            Console.WriteLine("Pasirinkite tipa: [1] po 10Eur, [2] po 20Eur, [3] po 30Eur");
+            Console.WriteLine("Iveskite kieki");
+            Console.WriteLine("[2] Kurti bilietus");
+            Console.WriteLine("Pasirinkite tipa: [1] po 10Eur, [2] po 20Eur, [3] po 30Eur");
+            Console.WriteLine("Iveskite kieki");
+            Console.WriteLine("[3] Ataskaita");
+        }
+        int kurtiBilietus(int bilietai, int rusis)
+        {
+            return 0;
+        }
+        int parduotiBilietus(int bilietai, int rusis)
+        {
+            return 0;
+        }
+
+    }
+}
