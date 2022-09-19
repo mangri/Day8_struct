1dad07d6c82a7557557e71f2f715cc01768d6b42
mangri <112893833+mangri@users.noreply.github.com>
2022-09-19 19:09:00 +0200

    Update structStudentList.cs

--- structStudentList.cs
+++ structStudentList.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using static structStudentList.Program;
 
 namespace structStudentList
 {
@@ -8,23 +9,25 @@ namespace structStudentList
         static void Main(string[] args)
         {
             //Adding five students to the list StudentList
-            List<string> StudentList = new List<string>();
-            for(int i = 1; i < 6; i++)
-            {
-                StudentList.Add("Student" + Convert.ToString(i));
-            }
-            //Putting student IDs and results
-            Student Student1 = new Student("PoPeYe314", true);
-            Student Student2 = new Student("Tulipo215", true);
-            Student Student3 = new Student("Salala777", true);
-            Student Student4 = new Student("Tartar046", false);
-            Student Student5 = new Student("Megalo666", false);
-            //Printing student info
+            List<Student> StudentList = new List<Student>();
+
+            Student student1 = new Student("PoPeYe314", true);
+            Student student2 = new Student("Tulipo215", true);
+            Student student3 = new Student("Salala777", true);
+            Student student4 = new Student("Tartar046", false);
+            Student student5 = new Student("Megalo666", false);
+
+            StudentList.Add(student1);
+            StudentList.Add(student2);
+            StudentList.Add(student3);
+            StudentList.Add(student4);
+            StudentList.Add(student5);
+
             showingStudents(StudentList);
             
 
         }
-        struct Student
+        public struct Student
         {
             public string Id;
             public bool ExamPass;
@@ -35,14 +38,22 @@ namespace structStudentList
                 Id = id;
                 ExamPass = examPass;
             }
+            
+
         }
-        static void showingStudents(List<string> StudentList)
+        static void showingStudents(List<Student> StudentList)
         {
-            Console.WriteLine("Here is the student list:");
-            foreach(var st in StudentList)
+
+            
+            foreach (Student student in StudentList)
             {
-                Console.WriteLine(st);
+                Console.Write(student.Id);
+                Console.Write(student.ExamPass);
+                Console.WriteLine();
             }
+
         }
+
+
     }
 }
